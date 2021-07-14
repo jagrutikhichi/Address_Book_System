3c601e7f1deffae72fff83d485ae1c5c3e3bbd24
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-14 20:09:37 +0530

    Initial Setup

---
+++ src/main/java/AddressBook/Contacts.java
@@ -0,0 +1,26 @@
+package AddressBook;
+
+public class Contacts {
+	String firstName;
+	String lastName;
+	String address;
+	String City;
+	String state;
+	Long zipCode;
+	Long phoneNumber;
+	String email;
+	public Contacts(String firstName, String lastName, String address, String city, String state, Long zipCode,
+			Long phoneNumber, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.City = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	
+	
+}
