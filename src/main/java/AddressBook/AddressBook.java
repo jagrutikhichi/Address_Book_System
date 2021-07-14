b1683e34d077e853fe002eb34f136da71c9943cf
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-14 20:37:05 +0530

    Added: Add contacts method

---
+++ src/main/java/AddressBook/AddressBook.java
@@ -0,0 +1,46 @@
+package AddressBook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+	Contacts contact = new Contacts();
+	Scanner sc = new Scanner(System.in);
+
+	public void add() {
+
+		
+		
+		//Takes input from user
+		
+		System.out.println("Enter First Name: ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter last Name: ");
+		String lastName = sc.nextLine();
+		System.out.println("Enter your address: ");
+		String address = sc.nextLine();
+		System.out.println("Enter your city: ");
+		String city = sc.nextLine();
+		System.out.println("Enter your state: ");
+		String state = sc.nextLine();
+		System.out.println("Enter zip code of your area: ");
+		Long zipCode = sc.nextLong();
+		sc.nextLine();
+		System.out.println("Enter phone number: ");
+		Long phoneNum = sc.nextLong();
+		sc.nextLine();
+		System.out.println("Enter your EMail ID: ");
+		String email = sc.nextLine();
+		
+		//calling the set method
+		
+		contact.setFirstName(firstName);
+		contact.setLastName(lastName);
+		contact.setAddress(address);
+		contact.setCity(city);
+		contact.setState(state);
+		contact.setZipCode(zipCode);
+		contact.setPhoneNumber(phoneNum);
+		contact.setEmail(email);
+		System.out.println(contact);
+	}
+}
--- src/main/java/AddressBook/AddressBookMain.java
+++ src/main/java/AddressBook/AddressBookMain.java
@@ -1,9 +1,19 @@
 package AddressBook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
+		AddressBook myObj = new AddressBook();
 		System.out.println("Welcome to Address Book System");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1.Add ");
+		int input = sc.nextInt();
+		switch (input) {
+		case 1:
+			myObj.add();
+		}
 	}
 
 }
