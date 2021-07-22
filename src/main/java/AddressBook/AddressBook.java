f40040e7abd3e1bd0796b4f2f429c5e81d5f4d5a
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-22 17:31:52 +0530

    Added: program to read and write file using IO

--- src/main/java/AddressBook/AddressBook.java
+++ src/main/java/AddressBook/AddressBook.java
@@ -1,5 +1,6 @@
 package AddressBook;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -16,7 +17,6 @@ public class AddressBook {
 	public static Map<String, Contacts> nameHashMap = new HashMap<String, Contacts>();
 	public static Map<String, Contacts> cityHashMap = new HashMap<String, Contacts>();
 	public static Map<String, Contacts> stateHashMap = new HashMap<String, Contacts>();
-
 	static Scanner sc = new Scanner(System.in);
 	static AddressBook addressBook = new AddressBook();
 
@@ -146,7 +146,6 @@ public class AddressBook {
 			switch (choice) {
 			case 1:
 				if (addContact(readContact())) // call addcontact with passing method readcontact
-
 					System.out.println("Cotact Added Successfully....!");
 				else
 					System.out.println("Contact Already Exist....!");
@@ -271,7 +270,7 @@ public class AddressBook {
 		switch (choice) {
 		case 1:
 			Map<String, Long> countCity = contactList.stream()
-					.collect(Collectors.groupingBy(e-> e.getCity(), Collectors.counting()));
+					.collect(Collectors.groupingBy(e -> e.getCity(), Collectors.counting()));
 			System.out.println(countCity + "\n");
 			break;
 		case 2:
@@ -302,7 +301,8 @@ public class AddressBook {
 			addressBook.sortBy(Contacts::getFirstName).forEach(System.out::println);
 			break;
 		case 2:
-			addressBook.sortBy(Contacts::getFirstName).forEach(System.out::println);
+			addressBook.sortBy(Contacts::getLastName).forEach(System.out::println);
+		
 			break;
 		case 3:
 			addressBook.sortBy(Contacts::getCity).forEach(System.out::println);
